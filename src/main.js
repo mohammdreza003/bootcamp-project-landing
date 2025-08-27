4a75a7e73d8f984cb3737a96332e7726c22d31e8
sara <saraotesee@gmail.com>
2025-08-27 19:00:20 +0330

    inint header

--- src/main.js
+++ src/main.js
@@ -1,4 +1,38 @@
-let btn = document.querySelector("button")
-btn.addEventListener("click" , ()=> {
-    document.documentElement.classList.toggle("dark")
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdownBtn = document.getElementById("dropdownBtn");
+  const dropdownMenu = document.getElementById("dropdownMode");
+
+  dropdownBtn.addEventListener("click", () => {
+    if (window.innerWidth >= 1024) {
+      dropdownMenu.classList.toggle("hidden");
+    }
+  });
+
+  const tickedItem = document.querySelectorAll("#dropdownMode li");
+  const burgerImg = document.querySelector("header img");
+
+  tickedItem.forEach((li) => {
+    li.addEventListener("click", () => {
+      tickedItem.forEach((el) =>
+        el.querySelector("img:last-child").classList.add("hidden")
+      );
+      li.querySelector("img:last-child").classList.remove("hidden");
+
+      const img = li.querySelector("img:first-child");
+      if (img.src.endsWith("light-outline.png")) {
+        document.documentElement.classList.remove("dark");
+        burgerImg.src = "../assets/images/Frame 12160.png";
+      } else if (img.src.endsWith("dark-outline.png")) {
+        document.documentElement.classList.add("dark");
+        burgerImg.src = "../assets/images/Frame 121600.png";
+      }
+    });
+  });
+
+  
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 1024) {
+      dropdownMenu.classList.add("hidden"); 
+    }
+  });
+});
